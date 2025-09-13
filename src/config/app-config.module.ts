db45938ad01c10ddd0e5780dfa746b840cbf4051
Ilina Tsanova <107075825+Ilinats@users.noreply.github.com>
2025-09-13 12:35:14 +0300

    ops: configure database connection

---
+++ src/config/app-config.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true, // Когато хостна базата, да го сменя на false
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class AppConfigModule {}

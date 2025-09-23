6bc0b56cf41323b0eab0c1b21cf54d10c2d65760
Ilina Tsanova <107075825+Ilinats@users.noreply.github.com>
2025-09-23 16:50:57 +0300

    genres entity and dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AppConfigModule } from './config/app-config.module';
 import { UsersModule } from './users/users.module';
 import { MailModule } from './mail/mail.module';
 import { AuthModule } from './auth/auth.module';
+import { GenresModule } from './genres/genres.module';
+import { UserGenrePreferencesModule } from './genres/user-genre-preferences.module';
 
 @Module({
-  imports: [AppConfigModule, MailModule, AuthModule, UsersModule],
+  imports: [AppConfigModule, MailModule, AuthModule, UsersModule, GenresModule, UserGenrePreferencesModule],
   controllers: [AppController],
   providers: [AppService],
 })
